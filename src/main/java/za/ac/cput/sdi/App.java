1e7d5b73bf3aa9c1e22f560256c808e4ae09da6c
Michael <michaelbenjamin12.mb@outlook.com>
2021-03-30 03:01:29 +0200

    Initial set up of project

---
+++ src/main/java/za/ac/cput/sdi/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput.sdi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
